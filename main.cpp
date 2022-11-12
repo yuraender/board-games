8e07e6f0f331e11df402b32205949f059d9f54f5
YuraEnder <yuraender@yandex.ru>
2022-11-12 23:53:20 +0300

    Trying to make chessboard

--- main.cpp
+++ main.cpp
@@ -1,57 +1,122 @@
 #include <iostream>
 #include <windows.h>
 
-using namespace std;
-
 const int N = 8;
 
-int board[N];
-int queen = 0;
+//Символ фигуры как ASCII
+enum FigureType : char {
 
-bool check();
+    KING = 'K',
+    QUEEN = 'Q',
+    ROOK = 'R',
+    BISHOP = 'B',
+    KNIGHT = 'N',
+    PAWN = 'P',
+    EMPTY = ' '
+};
 
-void print();
+enum Color {
+
+    WHITE, BLACK
+};
+
+struct Figure {
+
+    FigureType type;
+    Color color;
+
+    bool Move(int row, int column, int newRow, int newColumn) {
+        //TODO: написать алгоритм для проверки перемещения
+        return false;
+    }
+
+    void Print() {
+        char ch = type;
+        if (color == BLACK) {
+            ch = std::tolower(ch);
+        }
+        std::cout << ch;
+    }
+};
+
+struct Cell {
+
+    Figure figure;
+    Color color;
+
+    void Print() {
+        if (figure.type != EMPTY) {
+            figure.Print();
+        } else {
+            std::cout << (color == WHITE ? ' ' : '*');
+        }
+    }
+};
+
+struct Board {
+    public:
+    Board() {
+        Initialize();
+    }
+
+    void SetFigure(int row, int column, FigureType type, Color color) {
+        Figure figure{};
+        figure.type = type;
+        figure.color = color;
+        cells[row][column].figure = figure;
+
+        //TODO: проверить, можно ли ставить (не убивается ли)
+    }
+
+    void Print() {
+        for (int row = N - 1; row >= 0; row--) {
+            std::cout << row + 1 << ' ';
+            for (int column = 0; column < N; column++) {
+                cells[row][column].Print();
+                std::cout << ' ';
+            }
+            std::cout << '\n';
+        }
+        std::cout << "  A B C D E F G H";
+    }
+
+    private:
+    //TODO: переделать на А5, E3 и т.д. (map)
+    Cell cells[N][N];
+
+    void Initialize() {
+        Color color = BLACK;
+        for (int row = 0; row < N; row++) {
+            for (int column = 0; column < N; column++) {
+                Cell cell{};
+                cell.color = color;
+                cells[row][column] = cell;
+                SetFigure(row, column, EMPTY, {});
+
+                if (column != N - 1) {
+                    color = color == WHITE ? BLACK : WHITE;
+                }
+            }
+        }
+    }
+
+    void Reset() {
+        for (int row = 0; row < N; row++) {
+            for (int column = 0; column < N; column++) {
+                SetFigure(row, column, EMPTY, {});
+            }
+        }
+    }
+};
 
 int main() {
-    //Set encoding to UTF-8 on Windows
     SetConsoleCP(65001);
     SetConsoleOutputCP(65001);
 
-    while (board[0] < N) {
-        if (check()) {
-            if (queen == N - 1) {
-                print();
-                board[queen]++;
-            } else {
-                queen++;
-            }
-            continue;
-        }
-        board[queen]++;
-    }
-
+    Board board = Board{};
+//    board.SetFigure(3, 4, QUEEN, WHITE);
+//    board.SetFigure(5, 5, BISHOP, BLACK);
+//    board.SetFigure(7, 0, KNIGHT, WHITE);
+    board.Print();
     return 0;
-}
-
-bool check() {
-    for (int i = 0; i < queen; i++) {
-        if (board[queen] == board[i]
-            || board[queen] - board[i] == queen - i
-            || board[i] - board[queen] == queen - i) {
-            return false;
-        }
-    }
-    if (board[queen] > N - 1) {
-        board[queen] = 0;
-        queen--;
-        return false;
-    }
-    return true;
-}
-
-void print() {
-    for (int i : board) {
-        cout << i + 1 << ' ';
-    }
-    cout << '\n';
 }
