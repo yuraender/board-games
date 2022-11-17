d4c49a300df95e712d690279d1f63df016ae0074
YuraEnder <yuraender@yandex.ru>
2022-11-18 01:39:44 +0300

    Use map instead of array

--- main.cpp
+++ main.cpp
@@ -1,7 +1,10 @@
 #include <iostream>
+#include <map>
+#include <stdexcept>
 #include <windows.h>
 
 const int N = 8;
+const char FILES[N] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H'};
 
 //Символ фигуры как ASCII
 enum FigureType : char {
@@ -12,12 +15,30 @@ enum FigureType : char {
     BISHOP = 'B',
     KNIGHT = 'N',
     PAWN = 'P',
-    EMPTY = ' '
+    EMPTY = '\0'
 };
 
 enum Color {
 
     WHITE, BLACK
+};
+
+struct Position {
+
+    char file;
+    int rank;
+
+    Position(char file, int rank):
+            file(file),
+            rank(rank) {
+        if (file < 'A' || file > 'H' || rank < 1 || rank > 8) {
+            throw std::runtime_error("Incorrect position");
+        }
+    }
+
+    bool operator<(const Position& position) const {
+        return file < position.file || (file == position.file && rank < position.rank);
+    }
 };
 
 struct Figure {
@@ -59,20 +80,27 @@ struct Board {
         Initialize();
     }
 
-    void SetFigure(int row, int column, FigureType type, Color color) {
+    Figure SetFigure(Position position, FigureType type, Color color) {
         Figure figure{};
         figure.type = type;
         figure.color = color;
-        cells[row][column].figure = figure;
+        cells[position].figure = figure;
 
-        //TODO: проверить, можно ли ставить (не убивается ли)
+        if (type != EMPTY) {
+            //TODO: проверить, можно ли ставить (не убивается ли)
+        }
+        return figure;
+    }
+
+    void RemoveFigure(Position position) {
+        SetFigure(position, EMPTY, {});
     }
 
     void Print() {
-        for (int row = N - 1; row >= 0; row--) {
-            std::cout << row + 1 << ' ';
-            for (int column = 0; column < N; column++) {
-                cells[row][column].Print();
+        for (int rank = N; rank > 0; rank--) {
+            std::cout << rank << ' ';
+            for (const auto& file : FILES) {
+                cells[Position{file, rank}].Print();
                 std::cout << ' ';
             }
             std::cout << '\n';
@@ -81,29 +109,23 @@ struct Board {
     }
 
     private:
-    //TODO: переделать на А5, E3 и т.д. (map)
-    Cell cells[N][N];
+    std::map<Position, Cell> cells;
 
     void Initialize() {
-        Color color = BLACK;
-        for (int row = 0; row < N; row++) {
-            for (int column = 0; column < N; column++) {
+        for (const auto& file : FILES) {
+            for (int rank = 1; rank <= N; rank++) {
                 Cell cell{};
-                cell.color = color;
-                cells[row][column] = cell;
-                SetFigure(row, column, EMPTY, {});
-
-                if (column != N - 1) {
-                    color = color == WHITE ? BLACK : WHITE;
-                }
+                cell.color = (rank + file - 'A') % 2 == 0 ? WHITE : BLACK;
+                cells[Position{file, rank}] = cell;
+                SetFigure(Position{file, rank}, EMPTY, {});
             }
         }
     }
 
     void Reset() {
-        for (int row = 0; row < N; row++) {
-            for (int column = 0; column < N; column++) {
-                SetFigure(row, column, EMPTY, {});
+        for (const auto& file : FILES) {
+            for (int rank = 1; rank <= N; rank++) {
+                RemoveFigure(Position{file, rank});
             }
         }
     }
@@ -114,9 +136,10 @@ int main() {
     SetConsoleOutputCP(65001);
 
     Board board = Board{};
-//    board.SetFigure(3, 4, QUEEN, WHITE);
-//    board.SetFigure(5, 5, BISHOP, BLACK);
-//    board.SetFigure(7, 0, KNIGHT, WHITE);
+    board.SetFigure(Position{'E', 4}, QUEEN, WHITE);
+    board.SetFigure(Position{'F', 6}, BISHOP, BLACK);
+    board.SetFigure(Position{'A', 8}, KNIGHT, WHITE);
+
     board.Print();
     return 0;
 }
